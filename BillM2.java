f79cfe4e739c0e59fb94d881dea5e0811d35d2f0
SGIC-Batch4 <61105268+SGIC-Batch4@users.noreply.github.com>
2020-02-22 10:56:32 +0530

    modified

--- BillM2.java
+++ BillM2.java
@@ -1,11 +1,17 @@
 class Product{
+	private String id;
 	private String name;
 	private Double unitPrice;
 	
 
-	public Product(String name,Double unitPrice){
+	public Product(String id,String name,Double unitPrice){
+		this.id=id;
 		this.name=name;
 		this.unitPrice=unitPrice;
+	}
+	
+	public String getId(){
+		return this.id;
 	}
 
 	public String getName(){
@@ -20,15 +26,26 @@ class Product{
 }
 
 class Bill{
-	// assume 0 index for product
-	// assume 1 index for quantity
-	private Product[][] productsArray=new Product[5][2];
+	private String billno;
+	private String customerName;
+	private Product[] products=new Product[5];
+	private Integer[] quantities=new Integer[5];
+
 	private int productCounter=0;
 
-	public void purachaseProduct(Product product,Integer qty){
-		productsArray[productCounter][0]=product;
-		productsArray[productCounter][1]=qty;
+	Bill(String billno,String customerName){
+		this.billno=billno;
+		this.customerName=customerName;
+	}
+
+	public void purachaseProduct(Product product,Integer quantity){
+		this.products[productCounter]=product;
+		this.quantities[productCounter]=quantity;
 		productCounter=productCounter+1;
+	}
+
+	private Double calcualteSubtotal(int index){
+		return products[index].getUnitPrice()*quantities[index];
 	}
 
 	public Double getTotal(){
@@ -41,31 +58,38 @@ class Bill{
 		return total;
 	}
 
-	private Double calcualteSubtotal(int index){
-		return productsArray[index][0].getUnitPrice()*productsArray[index][1];
-	}
+	
 
 	public void printBill(){
+		System.out.println("-----------------------------------------------------------------------------");
+		System.out.println("Bill Number : "+this.billno);
+		System.out.println("Id\t|\tName\t|\tuprice\t|\tqty\t|\tsubtotal");
 		for(int i=0;i<productCounter;i++){
-			System.out.println(" | \t" + productsArray[i][0].getName() + "\t | \t"+ productsArray[i][0].getUnitPrice() 
-				+"\t | \t"+productsArray[i][1]+"\t | \t"+this.calcualteSubtotal(i));
+			System.out.println(products[i].getId()+"\t|\t" + products[i].getName() + "\t|\t"+ products[i].getUnitPrice() 
+				+"\t|\t"+quantities[i]+"\t|\t"+this.calcualteSubtotal(i));
 		}	
+		System.out.println("Total is : "+ this.getTotal());
+		System.out.println("-----------------------------------------------------------------------------");
 	}
 }
 
-class BillM2{
+class BillS1{
 	public static void main(String args[]){
-		Product pr1=new Product("Apple",50.00);
-		Product pr2=new Product("Orange",60.00);
-		Product pr3=new Product("Banana",10.00);
+		Product pr1=new Product("p001","Apple",50.00);
+		Product pr2=new Product("p002","Orange",60.00);
+		Product pr3=new Product("p003","Banana",10.00);
 
 
-		Bill billObj=new Bill();
-		billObj.purachaseProduct(pr1,2);
-		billObj.purachaseProduct(pr2,3);
-		billObj.purachaseProduct(pr3,4);
+		Bill billObj1=new Bill("001","kumar");
+		billObj1.purachaseProduct(pr1,2);
+		billObj1.purachaseProduct(pr2,3);
+		billObj1.printBill();
 
-		billObj.printBill();
-		System.out.println("Total is : "+ billObj.getTotal());
+		Bill billObj2=new Bill("002","ravi");
+		billObj2.purachaseProduct(pr3,10);
+		billObj2.purachaseProduct(pr2,3);
+		billObj2.purachaseProduct(pr1,2);
+		billObj2.printBill();
+		
 	}
 }
